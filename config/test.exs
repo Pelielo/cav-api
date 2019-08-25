ae51d090102870f8d7da1807fecc8c68d82eb916
pelielo <gabrielpelielo@hotmail.com>
2019-08-25 11:50:35 -0300

    fix: added back test config to use connection pool
    Signed-off-by: pelielo <gabrielpelielo@hotmail.com>

--- config/test.exs
+++ config/test.exs
@@ -1,12 +1,12 @@
 use Mix.Config
 
 # Configure your database
-# config :cav, Cav.Repo,
-#   username: "postgres",
+config :cav, Cav.Repo,
+  username: "postgres",
 #   password: "postgres",
 #   database: "postgres",
 #   hostname: "db",
-#   pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
